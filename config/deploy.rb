18ff19f3928a3cb795cd57e7064ccde063d1bb95
Helloworld-0703 <h.ryohei0703@gmail.com>
2019-12-20 17:24:58 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :application, 'i_sharing_app'
 set :repo_url, 'git@github.com:Hellowold-0703/i_sharing_app.git'
 
 
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads','config/master.key')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1'
