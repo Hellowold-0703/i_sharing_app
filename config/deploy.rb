c0e48daab07cedbcdc11764d61a670cbffaf71d3
Helloworld-0703 <h.ryohei0703@gmail.com>
2019-12-22 21:54:59 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :application, 'i_sharing_app'
 set :repo_url, 'git@github.com:Hellowold-0703/i_sharing_app.git'
 
 
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads', 'config/master.key')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 
 set :rbenv_type, :user
@@ -19,7 +19,7 @@ set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
-set :linked_files, %w{ config/credentials.yml.enc }
+set :linked_files, %w{ config/master.key }
 
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
@@ -27,13 +27,13 @@ namespace :deploy do
     invoke 'unicorn:restart'
   end
 
-  desc 'upload credentials.yml.enc'
+  desc 'upload master.key'
   task :upload do
     on roles(:app) do |host|
       if test "[ ! -d #{shared_path}/config ]"
         execute "mkdir -p #{shared_path}/config"
       end
-      upload!('config/credentials.yml.enc', "#{shared_path}/config/credentials.yml.enc")
+      upload!('config/master.key', "#{shared_path}/config/master.key")
     end
   end
   before :starting, 'deploy:upload'
