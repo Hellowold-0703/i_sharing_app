f7e5038dd778f84aedff12e42ccc12f1766ebcd9
Helloworld-0703 <h.ryohei0703@gmail.com>
2019-12-25 19:26:53 +0900

    本番環境で投稿を削除するとS３画像も削除されるように編集

--- app/models/image.rb
+++ app/models/image.rb
@@ -5,4 +5,14 @@ class Image < ApplicationRecord
   validates :image, presence: true
 
   mount_uploader :image, ImageUploader
+  before_destroy :remove_destroy
+
+  private
+
+  def remove_destroy
+    image.remove!
+    image.thumb.remove!
+  rescue => e
+    logger.error(e.message)
+  end
 end
